2f17a4419f4ebece9df055c76b879d9e50c8bb62
Eric Ahn <byungwook.ahn@gmail.com>
2020-07-09 01:59:36 +0900

    ..

--- stock-gen.py
+++ stock-gen.py
@@ -13,14 +13,22 @@ def yearTargetEst(ticker):
     return varYearTargetEst
 
 def currentValue(ticker):
-    #url = 'https://finance.yahoo.com/quote/SY?p=SY'
-    url = "https://finance.yahoo.com/quote/{}?p={}".format(ticker, ticker)
+    try:
+        #url = 'https://finance.yahoo.com/quote/SY?p=SY'
+        url = "https://finance.yahoo.com/quote/{}?p={}".format(ticker, ticker)
 
-    response = requests.get(url)
-    stock_content = BeautifulSoup(response.text, 'lxml')
-    stock_content = stock_content.find('div', {"class" : 'My(6px) Pos(r) smartphone_Mt(6px)'})
-    stock_content = stock_content.find('span').text
-    return stock_content
+        response = requests.get(url)
+        stock_content = BeautifulSoup(response.text, 'lxml')
+        stock_content = stock_content.find('div', {"class" : 'My(6px) Pos(r) smartphone_Mt(6px)'})
+        stock_content = stock_content.find('span').text
+
+    except AttributeError as error:
+        print("N/A 1", str(error).rstrip("\n"))
+    except Exception as exception:
+        print("N/A 2", str(exception).rstrip("\n"))
+    finally:
+        return stock_content
+
 
 df = pd.read_excel('./health-bio-vol-over-1.2m-over-0.5d.xlsx')
 # print(df['ticker'][0])
