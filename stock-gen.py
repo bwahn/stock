9f1cda55c43f68b77e071968a96e674389342e79
Eric Ahn <byungwook.ahn@gmail.com>
2020-07-08 22:40:01 +0900

    initial commit.

---
+++ stock-gen.py
@@ -0,0 +1,31 @@
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def yearTargetEst(ticker):
+    # url = 'https://finance.yahoo.com/quote/CHEK?p=CHEK'
+    url = "https://finance.yahoo.com/quote/{}?p={}".format(ticker, ticker) 
+
+    r = requests.get(url)
+    stock_content = BeautifulSoup(r.text, 'lxml')
+
+    varYearTargetEst = stock_content.find_all('td', {'data-test':'ONE_YEAR_TARGET_PRICE-value'})[0].find('span').text
+    return varYearTargetEst
+
+def currentValue(ticker):
+    #url = 'https://finance.yahoo.com/quote/SY?p=SY'
+    url = "https://finance.yahoo.com/quote/{}?p={}".format(ticker, ticker)
+
+    response = requests.get(url)
+    stock_content = BeautifulSoup(response.text, 'lxml')
+    stock_content = stock_content.find('div', {"class" : 'My(6px) Pos(r) smartphone_Mt(6px)'})
+    stock_content = stock_content.find('span').text
+    return stock_content
+
+df = pd.read_excel('./health-bio-vol-over-1.2m-over-0.5d.xlsx')
+# print(df['ticker'][0])
+for x in range(100):
+    # print(df['ticker'][x], currentValue(df['ticker'][x]))
+    print(currentValue(df['ticker'][x]))
+#  print(yearTargetEst(df['ticker'][x]))
+
